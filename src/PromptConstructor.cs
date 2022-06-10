9c83f6e04645e025116af0de4aad36de97db9ae8
Marcello Lamonaca <marcello@lamonaca.eu>
2022-06-10 16:30:57 +0200

    Split types in dedicated files

---
+++ src/PromptConstructor.cs
@@ -0,0 +1,55 @@
+using System.Text;
+using Spectre.Console;
+
+static class PromptConstructor
+{
+    const int ScriptListSize = 15;
+
+    private static Style SelectionHighlight => new(decoration: Decoration.Bold | Decoration.Underline);
+
+    private static string FileStyle(FileInfo info, bool brief)
+    {
+        var builder = new StringBuilder();
+
+        if (!brief)
+        {
+            builder.Append($"[blue]{info.DirectoryName}{Path.DirectorySeparatorChar}[/]");
+        }
+
+        builder
+            .Append($"[orangered1]{Path.GetFileNameWithoutExtension(info.Name)}[/]")
+            .Append($"[greenyellow]{info.Extension}[/]");
+
+        return builder.ToString();
+    }
+
+    private static string DirectoryStyle(DirectoryInfo info) => $"[blue]{info}[/]";
+
+    public static MultiSelectionPrompt<FileInfo> GetScriptPrompt(FileInfo[] files, bool brief)
+    {
+        var prompt = new MultiSelectionPrompt<FileInfo>()
+        .Title("Select a script the scripts to execute:")
+        .NotRequired()
+        .PageSize(ScriptListSize)
+        .InstructionsText("[grey](Press [blue]<space>[/] to toggle a script, [green]<enter>[/] to accept)[/]")
+        .MoreChoicesText("[grey]Move up and down to reveal more options[/]")
+        .UseConverter(x => FileStyle(x, brief))
+        .HighlightStyle(SelectionHighlight)
+        .AddChoices(files);
+
+        return prompt;
+    }
+
+    public static SelectionPrompt<DirectoryInfo> GetDirectoryPrompt(DirectoryInfo[] directories)
+    {
+        var prompt = new SelectionPrompt<DirectoryInfo>()
+        .Title("Select a directory:")
+        .PageSize(ScriptListSize)
+        .MoreChoicesText("[grey]Move up and down to reveal more options[/]")
+        .UseConverter(DirectoryStyle)
+        .HighlightStyle(SelectionHighlight)
+        .AddChoices(directories);
+
+        return prompt;
+    }
+}
---
+++ src/ScriptFinder.cs
@@ -0,0 +1,52 @@
+using Spectre.Console;
+
+readonly struct ScriptFinder
+{
+    static readonly string[] DefaultExtensions = new[] { ".ps1", ".*sh", ".bat", ".cmd" };
+    public string[] Extensions { get; }
+    public string RootDirectory { get; }
+    public int Depth { get; }
+    private readonly EnumerationOptions _options;
+
+    public ScriptFinder(string? extensions, string directory, int depth)
+    {
+        Extensions = extensions?.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .ToHashSet()
+        .Select(x => $".{x.TrimStart('.')}")
+        .ToArray() ?? DefaultExtensions;
+
+        Depth = depth;
+        RootDirectory = directory;
+
+        _options = new EnumerationOptions
+        {
+            IgnoreInaccessible = true,
+            RecurseSubdirectories = Depth > 0,
+            MaxRecursionDepth = Depth,
+        };
+    }
+
+    private IEnumerable<FileInfo> GetScriptFilesWithExtension(string extension)
+    {
+        try
+        {
+            var filenames = Directory.GetFiles(RootDirectory, $"*{extension}", _options);
+            return filenames.Select(x => new FileInfo(x));
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return Enumerable.Empty<FileInfo>();
+        }
+    }
+
+    public FileInfo[] GetScripts() =>
+        Extensions.Select(GetScriptFilesWithExtension).SelectMany(x => x).ToArray();
+
+    public IDictionary<DirectoryInfo, FileInfo[]> GetScriptsByDirectory() =>
+        Extensions
+        .Select(GetScriptFilesWithExtension)
+        .SelectMany(x => x)
+        .GroupBy(x => x.DirectoryName!)
+        .OrderBy(x => x.Key)
+        .ToDictionary(x => new DirectoryInfo(x.Key), x => x.ToArray());
+}
